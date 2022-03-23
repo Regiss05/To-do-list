4ab73d57cedca71d250c03c38923366a26d97937
regiss05 <regiss.steven@gmail.com>
2022-03-23 21:55:04 +0200

    fix lint errors

--- src/index.js
+++ src/index.js
@@ -1,34 +1,4 @@
 import './style.css';
-// import renderlists from './modules/renderList.js';
-
-// const form = document.createElement('form');
-// form.classList.add('list-container');
-
-// const orderedList = document.createElement('ul');
-// const titleList = document.createElement('li');
-// titleList.innerText = 'Todays To Do';
-
-// const addList = document.createElement('li');
-// addList.classList.add('input-data');
-
-// it will be assigned inside the addlist
-// const inputList = document.createElement('input');
-// text/placeholder
-
-// const listItemsList = document.createElement('div');
-// const orderedDynamic = document.createElement('ul');
-// orderedDynamic.classList.add('list-detail');
-
-// const listButton = document.createElement('li');
-// const buttonList = document.createElement('button');
-
-// form.appendChild(orderedList);
-// orderedList.appendChild(titleList);
-// orderedList.appendChild(addList);
-// orderedList.appendChild(listItemsList);
-// listItemsList.appendChild(orderedDynamic);
-// orderedList.appendChild(listButton);
-// listButton.appendChild(buttonList);
 
 const lists = [
   {
